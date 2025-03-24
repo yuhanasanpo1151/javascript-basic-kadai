59beddcc71a00d02dd08aa49c6d472a33be9a6f1
yuhanasanpo1151 <keamaneus@yahoo.co.jp>
2025-03-24 19:39:10 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = today.getFullYear();
+const month = today.getMonth();
+const day = today.getDate();
+
+const formattedDate = `${2024}年${10}月${22}日`;
+
+console.log(formattedDate);
